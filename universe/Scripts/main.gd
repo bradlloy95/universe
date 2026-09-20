ac86840732905a85d71cbaa28cf52f71c6a13229
Squiffy Shrimp <bradlloyd95@hotmail.com>
2026-09-20 16:24:23 +0100

    added collisions, mass and brighness and camera follows cent of mass

--- universe/Scripts/main.gd
+++ universe/Scripts/main.gd
@@ -2,27 +2,46 @@ extends Node2D
 
 # load particle scene
 var particle_scene = preload("res://Scenes/particle.tscn")
-@export var particles_created := 100
+@export var particles_created := 150
 
-var centre_screen : Vector2
+var centre_screen := Vector2(576, 324)
 
 var flash = false
 
-@export var gravity_strength = 1000.0
+@export var gravity_strength = 500.0
 @export var minimum_gravity_distance = 100.0
+@export var collision_temperature := 950.0
+@export var collision_heat := 50.0
+
+
+var collision_detection_enabled = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	centre_screen = get_viewport().get_visible_rect().size/2
+	#centre_screen = get_viewport().get_visible_rect().size/2
 	print(gravity_strength)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	var particles = $Particles.get_children()
+	if particles.size() > 0:
+		var centre_of_mass = get_centre_of_mass(particles)
+		$Camera2D.position = $Camera2D.position.lerp(centre_of_mass, 2.0 * delta)
 	
 	for i in range(particles.size()):
 		for j in range(i + 1, particles.size()):
 			calculate_gravity(particles[i], particles[j])
+			
+			if particles[i].merging or particles[j].merging:
+				continue
+			
+			if particles_are_touching(particles[i],particles[j]):
+				if particles[i].temperature < collision_temperature and particles[j].temperature < collision_temperature:
+					particles[i].merging = true
+					particles[j].merging = true 
+					
+					merge_particles(particles[i], particles[j])
+				
 
 
 func _on_button_pressed() -> void:
@@ -33,7 +52,7 @@ func _on_button_pressed() -> void:
 	for i in range(particles_created):
 		
 		var particle = particle_scene.instantiate()
-		particle.position = centre_screen
+		particle.position = get_start_position()
 		particle.velocity = get_velocity()
 		particle.mass = get_rand_mass()
 		$Particles.add_child(particle)
@@ -49,9 +68,11 @@ func get_velocity():
 func get_rand_mass():
 	return randf_range(0.5, 2.0)
 
+func get_start_position():
+	return centre_screen + Vector2.from_angle(randf() * TAU) * randf_range(0.0, 100.0)
 func _draw() -> void:
 	if flash:
-		draw_circle(centre_screen, 30, Color.WHITE)
+		draw_circle(centre_screen, 50, Color.WHITE)
 
 func _on_flash_timer_timeout() -> void:
 	flash = false
@@ -81,3 +102,36 @@ func calculate_gravity(particle_a, particle_b):
 	particle_a.apply_force(gravity_force / particle_a.mass)
 	# we add minus so its pulled towards A
 	particle_b.apply_force(-gravity_force / particle_b.mass)
+
+func particles_are_touching(particle_a, particle_b):
+	var distance = particle_a.position.distance_to(particle_b.position)
+	
+	return distance < particle_a.radius + particle_b.radius
+
+func merge_particles(particle_A, particle_B):
+	var new_particle = particle_scene.instantiate()
+	new_particle.position = (particle_A.position + particle_B.position)/ 2.0
+	new_particle.mass = particle_A.mass + particle_B.mass
+	new_particle.temperature = (particle_A.temperature + particle_B.temperature) / 2.0 + collision_heat
+	#new_particle.radius = sqrt(new_particle.mass) * 4.0
+	new_particle.velocity = (
+		particle_A.velocity * particle_A.mass + 
+		particle_B.velocity * particle_B.mass) / new_particle.mass
+	$Particles.add_child(new_particle)
+	
+	particle_A.queue_free()
+	particle_B.queue_free()
+	
+func get_centre_of_mass(particles):
+	var total_mass = 0.0
+	var weightrd_position = Vector2.ZERO
+	
+	for particle in particles:
+		total_mass += particle.mass
+		weightrd_position += particle.position * particle.mass
+		
+	
+	if total_mass == 0:
+		return Vector2.ZERO
+	
+	return weightrd_position / total_mass
--- universe/Scripts/particle.gd
+++ universe/Scripts/particle.gd
@@ -2,15 +2,42 @@ extends Node2D
 
 var velocity = Vector2.ZERO
 var mass := 1.0
+var touching = false
+var merging = false
+@export var temperature := 1000.0
+@export var radius := 4.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	queue_redraw()
+	
 func _process(delta: float) -> void:
 	position += velocity * delta
+	temperature -= 15.0 * delta / mass
+	queue_redraw()
+	
 
 func _draw() -> void:
-	draw_circle(Vector2.ZERO, 4, Color.WHITE)
+	var heat = clamp(temperature / 1000.0, 0.0, 1.0)
+	
+	var particle_color : Color
+	
+	if heat > 0.75:
+		particle_color = Color(1.0,1.0,1.0).lerp(Color(1.0,0.5,0.0), (1.0 - heat) / 0.25)
+	elif heat > 0.5:
+		particle_color = Color(1.0,0.5,0.0).lerp(Color(1.0,0.0,0.0), (0.75 - heat) / 0.25)
+	elif heat > 0.25:
+		particle_color = Color(1.0,0.0,0.0).lerp(Color(0.4,0.0,0.0), (0.5 - heat) / 0.25)
+	else:
+		particle_color = Color(0.4,0.0,0.0).lerp(Color(0.05,0.05,0.05), (0.25 - heat) / 0.25)
+		
+		
+	var size = radius 
+	
+	if touching:
+		print("touching")
+		size = radius * 1.5
+	draw_circle(Vector2.ZERO, size, particle_color)
 
 func apply_force(force: Vector2):
 	velocity += force
