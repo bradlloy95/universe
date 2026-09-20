74434c2cef8d8688b603180522d44556cd4043de
Squiffy Shrimp <bradlloyd95@hotmail.com>
2026-09-20 12:16:20 +0100

    gravity added

--- universe/Scripts/main.gd
+++ universe/Scripts/main.gd
@@ -2,18 +2,27 @@ extends Node2D
 
 # load particle scene
 var particle_scene = preload("res://Scenes/particle.tscn")
+@export var particles_created := 100
 
 var centre_screen : Vector2
 
 var flash = false
 
+@export var gravity_strength = 1000.0
+@export var minimum_gravity_distance = 100.0
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	centre_screen = get_viewport().get_visible_rect().size/2
+	print(gravity_strength)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	var particles = $Particles.get_children()
+	
+	for i in range(particles.size()):
+		for j in range(i + 1, particles.size()):
+			calculate_gravity(particles[i], particles[j])
 
 
 func _on_button_pressed() -> void:
@@ -21,27 +30,54 @@ func _on_button_pressed() -> void:
 	queue_redraw()
 	$FlashTimer.start()
 	$Button.queue_free()
-	for i in range(100):
+	for i in range(particles_created):
 		
 		var particle = particle_scene.instantiate()
 		particle.position = centre_screen
-		particle.velocity = Vector2(
-			get_x_velocity(),
-			get_y_velocity()
-		)
-		add_child(particle)
+		particle.velocity = get_velocity()
+		particle.mass = get_rand_mass()
+		$Particles.add_child(particle)
 
-func get_x_velocity():
-	return randf_range(-100.0, 100.0)
 
-func get_y_velocity():
-	return randf_range(-100.0, 100.0)
+#=============== particle functions =====================
+func get_velocity():
+	return Vector2(
+		randf_range(-100.0, 100.0),
+		randf_range(-100.0, 100.0)
+	)
+
+func get_rand_mass():
+	return randf_range(0.5, 2.0)
 
 func _draw() -> void:
 	if flash:
 		draw_circle(centre_screen, 30, Color.WHITE)
 
-
 func _on_flash_timer_timeout() -> void:
 	flash = false
 	queue_redraw()
+
+# ===================== PhYSICS ========================
+
+func calculate_gravity(particle_a, particle_b):
+	var direction = particle_b.position - particle_a.position
+	var distance = direction.length()
+	
+	# if too close do nothing as will be too strong
+	if distance < minimum_gravity_distance:
+		return 
+	
+	# bigger masses = stronger gravity
+	# greater distamce  = weaker gravity
+	var force = gravity_strength * particle_a.mass * particle_b.mass / (distance* distance)
+	
+	# direction.normalized() gives vector pointing towards b with len of exact 1
+	var gravity_force = direction.normalized() * force
+	
+	# we divide by A's mass because:
+	# force = mass x acceleration
+	# therefore:
+	#acceleration = force / mass
+	particle_a.apply_force(gravity_force / particle_a.mass)
+	# we add minus so its pulled towards A
+	particle_b.apply_force(-gravity_force / particle_b.mass)
--- universe/Scripts/particle.gd
+++ universe/Scripts/particle.gd
@@ -1,5 +1,7 @@
 extends Node2D
+
 var velocity = Vector2.ZERO
+var mass := 1.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -9,3 +11,6 @@ func _process(delta: float) -> void:
 
 func _draw() -> void:
 	draw_circle(Vector2.ZERO, 4, Color.WHITE)
+
+func apply_force(force: Vector2):
+	velocity += force
